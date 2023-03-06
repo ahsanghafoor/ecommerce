def382e9ff419557cf3b01295ab7e01e0f07c6ff
ahsanghafoor <ahsanghafoor2016@gmail.com>
2023-03-07 00:02:26 +0500

    Ecommerce

---
+++ frontend/src/actions/userActions.js
@@ -0,0 +1,37 @@
+import axios from "axios";
+import {
+  USER_LOGIN_REQUEST,
+  USER_LOGIN_SUCCESS,
+  USER_LOGIN_FAIL,
+} from "../constants/userConstants";
+export const login = (email, password) => async (dispatch) => {
+  //thunk
+  try {
+    dispatch({
+      type: USER_LOGIN_REQUEST,
+    });
+    const config = {
+      headers: {
+        "content-type": "application/json",
+      },
+    };
+    const { data } = await axios.post(
+      "/api/users/login",
+      { email, password },
+      config
+    );
+    dispatch({
+      type: USER_LOGIN_REQUEST,
+      paylaod: data,
+    });
+    localStorage.setItem("userInfo", JSON.stringify(data));
+  } catch (error) {
+    dispatch({
+      type: USER_LOGIN_FAIL,
+      payload:
+        error.responce && error.responce.data.message
+          ? error.responce.data.message
+          : error.message,
+    });
+  }
+};
